33f09f0623d206556dcb7b4c6df97b14812a5e2c
dondycles <johnrod.dondoyano@gmail.com>
2026-02-20 18:25:40 +0800

    feat: add initial landing page showcasing musician and web developer profile with integrated music stores and social links.

--- src/lib/music-stores.ts
+++ src/lib/music-stores.ts
@@ -1,5 +1,15 @@
 export const MUSIC_STORES = [
   {
+    name: 'Tidal',
+    url: 'https://tidal.com/artist/19629668',
+    icon: "data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20239.5%20159.7'%20id='tidal-diamond'%3e%3cpath%20d='M159.67%2039.938l-39.88%2039.88-39.88-39.88%2039.88-39.88zM159.669%20119.756l-39.88%2039.88-39.88-39.88%2039.88-39.88zM79.809%2039.912l-39.88%2039.88-39.88-39.88%2039.88-39.88zM239.505%2039.93l-39.88%2039.88-39.88-39.88%2039.88-39.88z'/%3e%3c/svg%3e",
+  },
+  {
+    name: 'Deezer',
+    url: 'https://www.deezer.com/en/artist/94847662',
+    icon: 'https://www.deezer.com/favicon.ico',
+  },
+  {
     name: 'Spotify',
     url: 'https://open.spotify.com/artist/2opN2BRNgnBJIO932Kyr3U',
     icon: 'https://open.spotify.com/favicon.ico',
@@ -9,24 +19,15 @@ export const MUSIC_STORES = [
     url: 'https://music.apple.com/us/artist/john-rod-dondoyano/1716913960',
     icon: 'https://music.apple.com/favicon.ico',
   },
-  {
-    name: 'YouTube Music',
-    url: 'https://music.youtube.com/channel/UCKk0kHA3fmAkxoaqYj_2RSw',
-    icon: 'https://music.youtube.com/img/favicon_144.png',
-  },
+
   {
     name: 'Amazon Music',
     url: 'https://music.amazon.com/artists/B088P7883M/john-rod-dondoyano',
     icon: 'https://m.media-amazon.com/images/G/01/music/logo/1.0/smile_256x256.png',
   },
   {
-    name: 'Deezer',
-    url: 'https://www.deezer.com/en/artist/94847662',
-    icon: 'https://www.deezer.com/favicon.ico',
-  },
-  {
-    name: 'Tidal',
-    url: 'https://tidal.com/artist/19629668',
-    icon: "data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20239.5%20159.7'%20id='tidal-diamond'%3e%3cpath%20d='M159.67%2039.938l-39.88%2039.88-39.88-39.88%2039.88-39.88zM159.669%20119.756l-39.88%2039.88-39.88-39.88%2039.88-39.88zM79.809%2039.912l-39.88%2039.88-39.88-39.88%2039.88-39.88zM239.505%2039.93l-39.88%2039.88-39.88-39.88%2039.88-39.88z'/%3e%3c/svg%3e",
+    name: 'YouTube Music',
+    url: 'https://music.youtube.com/channel/UCKk0kHA3fmAkxoaqYj_2RSw',
+    icon: 'https://music.youtube.com/img/favicon_144.png',
   },
 ];
--- src/lib/socials.ts
+++ src/lib/socials.ts
@@ -1,13 +1,24 @@
 export const SOCIALS = [
   {
-    name: 'Youtube',
-    link: 'https://youtube.com/johnroddondoyano',
-    icon: 'https://www.youtube.com/s/desktop/2a3cb36e/img/favicon_144x144.png',
+    name: 'X',
+    link: 'https://x.com/dondycles',
+    icon: 'https://abs.twimg.com/favicons/twitter.3.ico',
   },
   {
     name: 'Tiktok',
     link: 'https://tiktok.com/@dondycles',
     icon: 'https://sf-static.tiktokcdn.com/obj/eden-sg/uhtyvueh7nulogpoguhm/tiktok-icon2.png',
+  },
+
+  {
+    name: 'Github',
+    link: 'https://github.com/dondycles',
+    icon: 'https://github.com/favicon.ico',
+  },
+  {
+    name: 'Youtube',
+    link: 'https://youtube.com/johnroddondoyano',
+    icon: 'https://www.youtube.com/s/desktop/2a3cb36e/img/favicon_144x144.png',
   },
 
   {
@@ -19,16 +30,5 @@ export const SOCIALS = [
     name: 'Instagram',
     link: 'https://instagram.com/dondycles',
     icon: 'https://static.cdninstagram.com/rsrc.php/v4/yI/r/VsNE-OHk_8a.png',
-  },
-  {
-    name: 'X',
-    link: 'https://x.com/dondycles',
-    icon: 'https://abs.twimg.com/favicons/twitter.3.ico',
-  },
-
-  {
-    name: 'Github',
-    link: 'https://github.com/dondycles',
-    icon: 'https://github.com/favicon.ico',
   },
 ];
